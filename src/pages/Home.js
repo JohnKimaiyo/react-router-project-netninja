e0fb1f04e621d2972f75bb2b33a150169aba74e1
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-18 07:34:37 -0800

    home component

---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h2>Welocme</h2>
+      <p>
+        KYIV (Reuters) - Blasts wounded two civilians and shattered several
+        hundred windows in the west Ukrainian city of Khmelnytskyi as Russia
+        fired missiles from the Black Sea on Saturday, Ukrainian officials said.
+        Ukraine's air force said Russia launched four Kalibr missiles from the
+        Black Sea, two of which were shot down by air defences. Two explosions
+        were heard in Khmelnytskyi, which lies 170 miles (274 km) west of Kyiv,
+        the regional governor said. Oleksandr Symchyshyn, the mayor of
+        Khmelnytskyi, said on national television that the explosions, which he
+        blamed Russia, had wounded two people, but their injuries were not
+        serious. "There are three damaged educational institutions, around ten
+        damaged high-rise apartment blocks. Around five hundred windows and
+        balconies have been destroyed," he said. Symchyshyn did not say if the
+        missiles had struck their targets, or what they were aiming for.
+      </p>
+      Home
+    </div>
+  );
+}
