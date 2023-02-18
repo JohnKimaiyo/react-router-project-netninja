39359a3806de2277d9d8374bbb96c9d00ee10ce7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-02-18 08:18:21 -0800

    careers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route,NavLink, Link } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home";
+import About from "./pages/About";
 function App() {
   return (
-    <div className="App">
-      <Main>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobrouter</h1>
+          <Link to="/">Home</Link>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+
+      <main>
         <Routes>
           <Routes path="/" element={<Home />} />
           <Route path="about" element={<About />} />
         </Routes>
-      </Main>
-    </div>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About Us </h2>
+      <p>
+        Shortly after air raid alerts were issued nationwide on Saturday
+        morning, authorities in several southern and eastern regions of Ukraine
+        warned of possible precautionary power outages to limit damage to the
+        grid in case of a strike. Russia, which invaded its neighbour nearly a
+        year ago, has been targeting Ukraine's energy networks with massed
+        missile salvos since last October. Ukraine's state nuclear company said
+        two Russian cruise missiles flew close to the Pivdennoukrainska nuclear
+        plant in southern Ukraine just before 08:30 AM local time (0630 GMT).
+      </p>
+    </div>
+  );
+}
